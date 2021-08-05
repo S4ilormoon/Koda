53abc9e51ca65d0a54f211acbe342ae772a8eeaa
S4ilormoon <thomasspencer025@gmail.com>
2021-08-05 00:31:38 +0000

    update content

--- src/yourdata.js
+++ src/yourdata.js
@@ -45,7 +45,7 @@ export default {
       imageSrc:
         "https://github.com/S4ilormoon",
       //Project URL - Add Your Project Url Here
-      url: "https://github.com/S4ilormoon",
+      url: "https://images.unsplash.com/photo-1502643746182-00f9a4c916c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2689&q=80",
     },
     {
       title: "Coming Soon", //Project Title - Add Your Project Title Here
@@ -164,9 +164,9 @@ export default {
 
   //   Promotion Section --------------------------
 
-  promotionHeading: "Heading",
+  promotionHeading: "Build Together",
   promotionPara:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    "Reach out.",
   // End Promotion Section -----------------
 
   //   Contact Section --------------
@@ -175,14 +175,7 @@ export default {
   social: [
     // Add Or Remove The Link Accordingly
     { img: githubIcon, url: "https://github.com/s4ilormoon" },
-    {
-      img: codepenIcon,
-      url: "https://www.codepen.com/",
-    },
-    {
-      img: dribbbleIcon,
-      url: "https://dribbble.com/",
-    },
+    
   ],
 
   // End Contact Section ---------------
